08d50b10fa015f3b6e7abc3650f139eb07b4f483
seyityavuz <seyityavuz@gmail.com>
2025-10-28 21:22:38 +0300

    Update selcuk.py

--- selcuk.py
+++ selcuk.py
@@ -41,7 +41,7 @@ def build_m3u8_links(base_stream_url, channel_ids):
         m3u8_links.append((cid, full_url))
     return m3u8_links
 
-def write_m3u_file(m3u8_links, filename="5.m3u", referer=""):
+def write_m3u_file(m3u8_links, filename="selcuksport.m3u", referer=""):
     if not os.path.exists(filename):
         print("⛔ Dosya bulunamadı. Yeni dosya oluşturulamaz çünkü eski içerik korunmalı.")
         return
