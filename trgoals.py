087b18dbcff5386100e268cbfca5cc9179f9ba3e
seyityavuz <seyityavuz@gmail.com>
2025-11-09 19:41:11 +0300

    Update trgoals.py

--- trgoals.py
+++ trgoals.py
@@ -3,11 +3,11 @@ import re
 import os
 
 def find_working_selcuksportshd(start=1825, end=1850):
-    print("🧭 Selcuksportshd domainleri taranıyor...")
+    print("🧭 trgoals domainleri taranıyor...")
     headers = {"User-Agent": "Mozilla/5.0"}
 
     for i in range(start, end + 1):
-        url = f"https://www.selcuksportshd{i}.xyz/"
+        url = f"https://trgoals{i}.xyz/"
         print(f"🔍 Taranıyor: {url}")
         try:
             response = requests.get(url, headers=headers, timeout=5)
@@ -41,7 +41,7 @@ def build_m3u8_links(base_stream_url, channel_ids):
         m3u8_links.append((cid, full_url))
     return m3u8_links
 
-def write_m3u_file(m3u8_links, filename="selcuk.m3u", referer=""):
+def write_m3u_file(m3u8_links, filename="trgoals.m3u", referer=""):
     lines = []
     if os.path.exists(filename):
         with open(filename, "r", encoding="utf-8") as f:
